e4c1904aeddd84fac008d27c4cd03488ea74f1da
Christopher Kalfas <chriskalfas@yahoo.com>
2019-01-24 22:40:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,9 +29,12 @@ var funkyFunction = function() {
 // NOTE: you only need to modify the code below this line.
 var theFunk =  function funkyFunction() 
 
+
  function funkyFunction(){
   return "FUNKY!"
 }
+
+
 
 
 
